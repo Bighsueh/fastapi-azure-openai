b431236bc3401d6d912e445d440108e356399c45
bighsueh <ss313021490@gmail.com>
2023-07-24 05:26:07 +0800

    修正os.environ.get()語法錯誤的問題

--- roleplay.py
+++ roleplay.py
@@ -8,8 +8,8 @@ import mongoStore as db
 
 openai.api_type = "azure"
 openai.api_version = "2023-03-15-preview"
-openai.api_base = os.getenv("OPENAI_ENDPOINT")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = os.environ.get("OPENAI_ENDPOINT")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = APIRouter()
 
